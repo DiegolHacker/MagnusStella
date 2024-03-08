69ef538c01dc9603a5eba9bdbee6e67436076f1e
Angie <A01705651@tec.mx>
2024-03-08 00:35:24 -0600

    pagina_resenas_dinamicas

--- controllers/renderHtml.js
+++ controllers/renderHtml.js
@@ -13,7 +13,6 @@ exports.get_usuarios = (request, response, next) => {
         { id: 4, nombre: "Diego Ricardo Alfaro", correo: "A01709971@tec.mx", rol: "Analítica", estado: "Activo" },
         { id: 5, nombre: "Pablo Hazael Hurtado", correo: "A01710778@tec.mx", rol: "Analítica", estado: "Inactivo" }
     ];
-
     response.render('usuarios', { usuarios: usuarios, titulo:"Usuarios" });
 };
 
@@ -22,8 +21,29 @@ exports.get_login = (request, response,next) =>{
     response.render("login")
 }
 
-exports.get_resenas = (request, response,next) => {
-    response.render("resenas", {
+exports.get_resenas_completas = (request, response,next) => {
+    response.render("resenas_completas", {
         titulo: 'Reseñas',
     })
+};
+exports.get_resenas = (request, response,next) => {
+    const resenas = [
+        { itemcode: "LB3231", title: "Cama Condesa-INDI", estrellas: 5 },
+        { itemcode: "LB2231", title: "Cama Anzures-INDI", estrellas: 3 },
+        { itemcode: "LU1001B2", title: "Luuna Colchón Basic 2-INDI", estrellas: 4 },
+        { itemcode: "LU1002B2", title: "Luuna Colchón Basic 2-MATRI", estrellas: 4 },
+        { itemcode: "LU1003B2", title: "Luuna Colchón Basic 2-QUEEN", estrellas: 3 },
+        { itemcode: "LU1004B2", title: "Luuna Colchón Basic 2-KING", estrellas: 5 },
+        { itemcode: "LB2304", title: "Cama Nuevo León-KING", estrellas: 2 },
+        { itemcode: "LB2303", title: "Cama Nuevo León-QUEEN", estrellas: 5 },
+        { itemcode: "LB2302", title: "Cama Nuevo León-MATRI", estrellas: 1 },
+        { itemcode: "LB2301", title: "Cama Nuevo León-INDI", estrellas: 4 },
+        { itemcode: "BL1004HS", title: "Luuna Colchón Blue High Support-KING", estrellas: 5 },
+        { itemcode: "BL1003HS", title: "Luuna Colchón Blue High Support-QUEEN", estrellas: 4 },
+        { itemcode: "BL1002HS", title: "Luuna Colchón Blue High Support-MATRI", estrellas: 4 },
+        { itemcode: "BL1001HS", title: "Luuna Colchón Blue High Support-INDI", estrellas: 3 },
+        { itemcode: "LU1004B3", title: "Luuna Colchón Basic air B3-KING", estrellas: 0 },
+        { itemcode: "LU1003B3", title: "Luuna Colchón Basic air B3-QUEEN", estrellas: 5 },
+    ];
+    response.render("resenas", { resenas: resenas, titulo:"Reseñas" });
 };
