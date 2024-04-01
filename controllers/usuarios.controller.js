62be0eda0b8bf100f6ec8c4dbf353706761e2b10
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-01 10:26:19 -0600

    autenticacion con login usando bcrypt.compare

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -9,56 +9,41 @@ exports.get_login = (request, response, next) => {
 
 exports.post_login = (request, response, next) => {
     const {email, password} = request.body;
-    console.log(request.body);
     if(!email || !password){
-        return response.render("login", {error: "Llena todos los campos"})
+        return response.render("login", {error: "Llena todos los campos"});
     }
 
-    Usuarios.findByEmail(email,password)
-        .then(({user,passwordMatch}) => {
-            if(user && passwordMatch){
-                request.session.isLoggedIn = true;
-                request.session.user = user;
-                return request.session.save(err => {
-                    response.redirect('/');
-                });
-            }else{
-                response.redirect('/login');
+    Usuarios.findByEmail(email)
+        .then(user => {
+            if (user) {
+                // Use bcrypt.compare to check if passwords match
+                console.log(user);
+                console.log(password)
+                console.log(user.user.contrasena)
+                bcrypt.compare(password, user.user.contrasena)
+                    .then(doMatch => {
+                        if (doMatch) {
+                            request.session.isLoggedIn = true;
+                            request.session.user = user;
+                            return request.session.save(err => {
+                                response.redirect('/');
+                            });
+                        } else {
+                            response.redirect('/users/login');
+                        }
+                    })
+                    .catch(err => {
+                        console.error('Error during login', err);
+                        response.render("login",{error: "Usuario no existe"});
+                    });
+            } else {
+                response.redirect('/users/login');
             }
         })
         .catch(err => {
-            console.error('Error durante el login',err);
-            response.redirect('/users/login')
-        })
-
-    // Usuarios.findByEmail(email)
-    //     .then(({ user, passwordMatch }) => {
-    //         if (user && passwordMatch) {
-    //             // Use bcrypt.compare to check if passwords match
-    //             bcrypt.compare(password, user.Contrasena)
-    //                 .then(doMatch => {
-    //                     if (doMatch) {
-    //                         request.session.isLoggedIn = true;
-    //                         request.session.user = user;
-    //                         return request.session.save(err => {
-    //                             response.redirect('/');
-    //                         });
-    //                     } else {
-    //                         response.redirect('/login');
-    //                     }
-    //                 })
-    //                 .catch(err => {
-    //                     console.error('Error during login', err);
-    //                     response.redirect('/login');
-    //                 });
-    //         } else {
-    //             response.redirect('/login');
-    //         }
-    //     })
-    //     .catch(err => {
-    //         console.error('Error during login', err);
-    //         response.redirect('/login');
-    //     });
+            console.error('Error during login', err);
+            response.redirect('/users/login');
+        });
 
 };
 
@@ -77,11 +62,10 @@ exports.get_signup =(request, response, next) => {
     response.render("signup")
 };
 
-let latestID = 6; //en este caso empiezo en 1 ya que ya se creo el primer usuario
+let latestID = 7; //en este caso empiezo en 1 ya que ya se creo el primer usuario
 
 exports.post_signup = (request,response,next) => {
     const {name, email, password} = request.body;
-    console.log(request.body);
     
     if(!name || !password || !email){
         return response.render("signup", {error: "Llena todos los campos"})
@@ -92,6 +76,7 @@ exports.post_signup = (request,response,next) => {
     const IdRol = Math.floor(Math.random() * 11);
 
     const usuarios = new Usuarios(name,email,password,idUser,IdRol);
+    console.log(usuarios)
     usuarios.save()
         .then(() =>{
             response.redirect('/users/login')
--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -19,12 +19,11 @@ module.exports = class Usuarios {
             Correo: this.email,
         }
 
-
         return bcrypt.hash(userData.Contrasena, 12)
             .then((hashedPassword) => {
                 userData.Contrasena = hashedPassword;
                 const values = Object.values(userData);
-                return db.execute('INSERT INTO usuario (idUsuario,IdRol,Nombre,Constraseña,Correo) VALUES (?,?,?,?,?)',values);
+                return db.execute('INSERT INTO usuario (idUsuario,IdRol,Nombre,Contrasena,Correo) VALUES (?,?,?,?,?)',values);
             })
             .then(([result]) => {
                 console.log('Usuario Guardado:', result);
@@ -36,44 +35,24 @@ module.exports = class Usuarios {
             });
     } 
 
-    static findByEmail(email,password){
-        console.log(email,password)
-        return db.execute('SELECT * FROM usuario WHERE Correo = ?',[email])
-            .then(([rows]) =>{
-                if(rows.length > 0){
-                    const user = rows[0];
 
-                    //comparar la contrasena 
-                    console.log('Hashed', user.Contrasena);
-                    console.log('Mail', user.Correo);
-                    //aqui .compare recibe un undefined
-                    return bcrypt.compare(password,user.Contrasena)
-                        .then(doMatch => {
-                            return{ user: user, passwordMatch: doMatch};
-                        })
+    static findByEmail(email) {
+        return db.execute('SELECT * FROM usuario WHERE Correo = ?', [email])
+            .then(([rows]) => {
+                if (rows.length > 0) {
+                    const userData = rows[0];
+                    console.log('Esto es lo que esta recuperando de la base de datos: ', userData)
+                    const user = new Usuarios(userData.Nombre, userData.Correo, userData.Contrasena, userData.idUsuario, userData.IDRol);
+                    console.log('Esto es lo que se va a retornar: ',user);
+                    return { user: user, passwordMatch: true }; // Return user data with passwordMatch true
                 }
-                return{user:null,passwordMatchs:false};
+                return { user: null, passwordMatch: false }; // Return null user and passwordMatch false if user not found
             })
             .catch(err => {
-                console.log("Error encontrando usuario en la base de datos",err);
+                console.error('Error fetching user by email from database:', err);
                 throw err;
-            })
+            });
     }
-
-    // static findByEmail(email) {
-    //     return db.execute('SELECT * FROM usuario WHERE Correo = ?', [email])
-    //         .then(([rows]) => {
-    //             if (rows.length > 0) {
-    //                 const user = rows[0];
-    //                 return { user: user, passwordMatch: true }; // Return user data with passwordMatch true
-    //             }
-    //             return { user: null, passwordMatch: false }; // Return null user and passwordMatch false if user not found
-    //         })
-    //         .catch(err => {
-    //             console.error('Error fetching user by email from database:', err);
-    //             throw err;
-    //         });
-    // }
 
 
     static fetchAll(){
