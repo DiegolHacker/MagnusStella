71ee08c740ab22f591b893204618571cbcff5ac8
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-02 11:36:36 -0600

    cambio menor a login y controlador render

--- controllers/renderHtml.js
+++ controllers/renderHtml.js
@@ -17,10 +17,6 @@ exports.get_usuarios = (request, response, next) => {
 };
 
 
-exports.get_login = (request, response, next) => {
-    response.render("login")
-}
-
 exports.get_resenas = (request, response,next) => {
     response.render("resenas", {
         titulo: 'Reseñas',
