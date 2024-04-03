cc8d752bfbdc27dddec1ec38981ac9428cf6366d
Angie <A01705651@tec.mx>
2024-04-02 23:07:17 -0600

    resenas_completas_preguntas

--- models/resenas.model.js
+++ models/resenas.model.js
@@ -26,37 +26,44 @@ module.exports = class Reviews {
         });
 }
 
-static fetchCompleto(idReview, callback){
-    const query = `
-    SELECT 
-        c.nombre AS nombre_cliente,
-        p.nombre AS nombre_producto,
-        r.Puntaje AS estrellas_resena,
-        r.Titulo AS titulo_resena,
-        r.Descripcion AS descripcion_resena,
-        r.idReview AS id_review_resena
-    FROM 
-        cliente c
-    JOIN 
-        venta v ON c.idCliente = v.Fk_Venta_Cliente
-    JOIN 
-        producto p ON v.Fk_Venta_Producto = p.idProducto
-    JOIN 
-        review r ON v.idVenta = r.Fk_Review_Venta
-    WHERE
-        r.idReview = ?`;
+    static fetchCompleto(idReview, callback){
+        const query = `
+        SELECT 
+            c.nombre AS nombre_cliente,
+            p.nombre AS nombre_producto,
+            r.Puntaje AS estrellas_resena,
+            r.Titulo AS titulo_resena,
+            r.Descripcion AS descripcion_resena,
+            r.idReview AS id_review_resena,
+            pre.Descripcion AS pregunta,
+            resp.Respuestas AS respuesta
+        FROM 
+            cliente c
+        JOIN 
+            venta v ON c.idCliente = v.Fk_Venta_Cliente
+        JOIN 
+            producto p ON v.Fk_Venta_Producto = p.idProducto
+        JOIN 
+            review r ON v.idVenta = r.Fk_Review_Venta
+        JOIN
+            respuestas resp ON r.idReview = resp.FK_Respuestas_Review 
+        JOIN
+            pregunta pre ON resp.Fk_Respuestas_Pregunta = pre.idPregunta
 
-    db.execute(query, [idReview])
-    .then(([rows]) => {
-        if(rows.length > 0) {
-            callback(null, rows[0]); // Devuelve solo la primera fila encontrada
-        } else {
-            callback(new Error('Review not found'), null);
-        }
-    })
-    .catch(err => {
-        console.error('Error fetching complete review from database:', err);
-        callback(err, null);
-    });
-}
+        WHERE
+            r.idReview = ?`;
+
+        db.execute(query, [idReview])
+        .then(([rows]) => {
+            if(rows.length > 0) {
+                callback(null, rows[0]); // Devuelve solo la primera fila encontrada
+            } else {
+                callback(new Error('Review not found'), null);
+            }
+        })
+        .catch(err => {
+            console.error('Error fetching complete review from database:', err);
+            callback(err, null);
+        });
+    }
 }
