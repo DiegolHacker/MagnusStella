f568f1bd8a68581ce42c41bf13e790e3568f4889
Diego <a01705506@tec.mx>
2024-04-10 15:02:50 -0600

    Menú y ventanas ayuda

--- controllers/ayuda.controller.js
+++ controllers/ayuda.controller.js
@@ -2,9 +2,58 @@ const Model = require('../models/ayuda.model');
 
 exports.get_ayuda = (request, response, next) => {
     const marca = request.params.marca;
-    response.render("ayuda", {
+    response.render("ayudas/ayuda", {
         titulo: 'Ayuda',
         marca: marca
-        // csrfToken: request.csrfToken()
+    });
+}
+
+
+
+exports.get_ayuda_login = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-login", {
+        titulo: 'Ayuda Login',
+        marca: marca
+    });
+}
+
+exports.get_ayuda_dashboard = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-dashboard", {
+        titulo: 'Ayuda Dashboard',
+        marca: marca
+    });
+}
+
+exports.get_ayuda_resenas = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-resenas", {
+        titulo: 'Ayuda Reseñas',
+        marca: marca
+    });
+}
+
+exports.get_ayuda_correos = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-correos", {
+        titulo: 'Ayuda Correos',
+        marca: marca
+    });
+}
+
+exports.get_ayuda_usuarios = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-usuarios", {
+        titulo: 'Ayuda Usuarios',
+        marca: marca
+    });
+}
+
+exports.get_ayuda_general = (request, response, next) => {
+    const marca = request.params.marca;
+    response.render("ayudas/ayuda-general", {
+        titulo: 'Ayuda General',
+        marca: marca
     });
 }
--- routes/ayuda.routes.js
+++ routes/ayuda.routes.js
@@ -8,4 +8,11 @@ const controladores = require("../controllers/ayuda.controller");
 
 router.get("/:marca", controladores.get_ayuda);
 
+router.get("/login/:marca", controladores.get_ayuda_login);
+router.get("/dashboard/:marca", controladores.get_ayuda_dashboard);
+router.get("/resenas/:marca", controladores.get_ayuda_resenas);
+router.get("/correos/:marca", controladores.get_ayuda_correos);
+router.get("/usuarios/:marca", controladores.get_ayuda_usuarios);
+router.get("/general/:marca", controladores.get_ayuda_general);
+
 module.exports = router;
