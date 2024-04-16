a0bfb0eee553501e187fbd6244e2172ea0b49dfc
Angie <A01705651@tec.mx>
2024-04-16 11:51:46 -0600

    correo editar preguntas

--- controllers/correos.controller.js
+++ controllers/correos.controller.js
@@ -51,8 +51,7 @@ exports.get_correos_editar = async (request, response, next) => {
         const opciones  = await Correos.emailopciones(idp);
         const to_opcion = await Correos.emailcountopcion(idp);
         const pregunta = await Correos.emailpregunta(idp);
-        console.log(pregunta);
-    
+        const marca = request.params.marca;
         // Ahora renderiza con los datos obtenidos
         response.render("editar_correos", {
             titulo: "Correos",
@@ -71,3 +70,37 @@ exports.get_correos_editar = async (request, response, next) => {
     }
 };
 
+exports.post_editar_correos = async (request, response, next) => {
+    try {
+        const pregunta = request.body.pregunta || "";
+        const idPregunta = request.body.idp || "";
+        const marca = request.body.marca || "";
+        const opciones = [];
+        const idOpciones = [];
+
+        // Recuperar las opciones y sus IDs del body
+        for (const key in request.body) {
+            if (key.startsWith("opcion_")) {
+                opciones.push(request.body[key]);
+                // Extraer el ID de la opción del nombre del campo
+                const opcionId = key.replace("opcion_", "");
+                idOpciones.push(opcionId);
+            }
+        }
+
+        // Guardar la pregunta modificada
+        await Correos.saveQuestionChanges(pregunta, idPregunta);
+
+        // Guardar las opciones modificadas
+        for (let i = 0; i < opciones.length; i++) {
+            await Correos.saveEmailChanges(opciones[i], idOpciones[i]);
+        }
+        console.log(marca);
+        console.log("Guardado exitosamente");
+        response.redirect('/emails/correos/' + marca);
+
+    } catch (error) {
+        console.log("Error al guardar:", error);
+        response.status(500).send("Error interno del servidor");
+    }
+};
--- routes/correos.routes.js
+++ routes/correos.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controladores = require("../controllers/correos.controller");
 
-router.get("/correos/:marca/editar/:pregunta_id", controladores.get_correos_editar);
+router.get("/correos/editar/:marca/:pregunta_id", controladores.get_correos_editar);
+router.post('/correos/editar/', controladores.post_editar_correos);
 router.get("/correos/:marca", controladores.get_correos);
 module.exports = router;
