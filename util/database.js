a815fa4b82a55ec9548192d1876851bd10879c08
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-16 18:01:50 -0600

    cambio mas pequenio aun

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ const conection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'MagnusStella',
-    password: 'waspy0hasCikpaqfyw',
+    password: 'janzypfypsab4cYgko',
 });
 
 module.exports = conection.promise();
