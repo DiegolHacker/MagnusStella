021c187e79e6f7a350e42d94c3317720ad03485d
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-16 21:46:07 -0600

    cambio a conexion

--- util/database.js
+++ util/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 // usar la conexion a la base de datos que vayamos a utilizar
 const conection = mysql.createPool({
     host: 'http://146.190.127.1/',
-    user: 'magnusstella',
+    user: 'magnusstellaip',
     database: 'magnusstella',
-    password: 'waspy0hasCikpaqfyw',
+    password: 'waspy0hasCik_paqFYW',
 });
 
 module.exports = conection.promise();
