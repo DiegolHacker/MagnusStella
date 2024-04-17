71eed667dfb97c60f91a8b35aa95de6e9d6557cb
Diego Alfaro Pinto <a01709971@tec.mx>
2024-04-17 21:00:57 +0000

    cambio a database.js

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // usar la conexion a la base de datos que vayamos a utilizar
 const conection = mysql.createPool({
-    host: 'http://146.190.127.1/',
+    host: '146.190.127.1',
     user: 'magnusstellaip',
     database: 'magnusstella',
     password: 'waspy0hasCik_paqFYW',
