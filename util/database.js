6779c8bfbc1f1de0fa87a8964ca840610ea85801
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-16 18:09:06 -0600

    cambio a database util

--- util/database.js
+++ util/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 // usar la conexion a la base de datos que vayamos a utilizar
 const conection = mysql.createPool({
     host: '146.190.127.1',
-    user: 'magnusstella',
-    database: 'MagnusStella',
-    password: 'waspy0hasCikpaqfyw',
+    user: 'root',
+    database: 'magnusstella',
+    password: 'janzypfypsab4cYgko',
 });
 
 module.exports = conection.promise();
