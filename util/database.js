c0156beb9975f0854f0fd9352f963a94e35a8054
Diego Alfaro Pinto <a01709971@tec.mx>
2024-04-18 17:06:23 +0000

    Cambio a database.js

--- util/database.js
+++ util/database.js
@@ -6,7 +6,7 @@ const conection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
-    password: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 });
 
 module.exports = conection.promise();
