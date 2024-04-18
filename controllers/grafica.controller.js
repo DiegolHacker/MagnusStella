23d4b35d474a1f09586eea455ab4f12a92ced998
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-18 11:46:15 -0600

    cambio a controlador grafica

--- controllers/grafica.controller.js
+++ controllers/grafica.controller.js
@@ -7,87 +7,45 @@ exports.get_dashboard = async (request, response, next) => {
     let producto = request.body.producto || '*'; //"AN1133V"
     let errorMessage = '';
 
-    if(producto !== "*"){
-        Model.search(producto)
-            .then(result => {
-                if(result.error !== undefined) {
-                    console.log(result.error);
-                    const errorMessage = result.error;
-                    producto = "*";
-                    Promise.all([Model.StarAvgLine(marca,categoria), Model.tasaDeRespuesta(marca,categoria,producto),Model.ReviewsSentxMonth(marca,categoria,producto),Model.StarAvgNumber(marca,categoria,producto)])
-                        .then(([averageScores, responseRates,reviewsSent,starAVGNum]) => {
-                            const roundedStarAVGNum = Math.round(starAVGNum * 100) / 100
-                            const roundedStarAVGNumComplementario = Math.round(100 - ((roundedStarAVGNum / 5) * 100));
-                            response.render("dashboard", {
-                                titulo: 'Dashboard',
-                                promedioPuntajes: averageScores,
-                                tasaDeRespuesta: responseRates,
-                                encuestasEnviadas: reviewsSent,
-                                numAVGEstrella: roundedStarAVGNum,
-                                porcentaje: roundedStarAVGNumComplementario,
-                                marca: marca,
-                                ruta: "/graphics/dashboard",
-                                errorProducto : errorMessage,
-                                permisos: request.session.permisos || []
-                            });
-                            })
-                            .catch(error => {
-                                console.error('Error obteniendo datos para el dashboard:', error);
-                                next(error); // Pasa el error al siguiente middleware para manejarlo
-                            });
-                }else{
-                    const errorMessage = result.error;
-                    Promise.all([Model.StarAvgLine(marca,categoria), Model.tasaDeRespuesta(marca,categoria,producto),Model.ReviewsSentxMonth(marca,categoria,producto),Model.StarAvgNumber(marca,categoria,producto)])
-                        .then(([averageScores, responseRates,reviewsSent,starAVGNum]) => {
-                            const roundedStarAVGNum = Math.round(starAVGNum * 100) / 100
-                            const roundedStarAVGNumComplementario = Math.round(100 - ((roundedStarAVGNum / 5) * 100));
-                            response.render("dashboard", {
-                                titulo: 'Dashboard',
-                                promedioPuntajes: averageScores,
-                                tasaDeRespuesta: responseRates,
-                                encuestasEnviadas: reviewsSent,
-                                numAVGEstrella: roundedStarAVGNum,
-                                porcentaje: roundedStarAVGNumComplementario,
-                                marca: marca || "LU1",
-                                ruta: "/graphics/dashboard",
-                                errorProducto : errorMessage,
-                                permisos: request.session.permisos || []
-                            });
-                        })
-                        .catch(error => {
-                            console.error('Error obteniendo datos para el dashboard:', error);
-                            next(error); // Pasa el error al siguiente middleware para manejarlo
-                        });
-                }
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }else{
-        Promise.all([Model.StarAvgLine(marca,categoria), Model.tasaDeRespuesta(marca,categoria,producto),Model.ReviewsSentxMonth(marca,categoria,producto),Model.StarAvgNumber(marca,categoria,producto)])
-            .then(([averageScores, responseRates,reviewsSent,starAVGNum]) => {
-                const roundedStarAVGNum = Math.round(starAVGNum * 100) / 100
-                const roundedStarAVGNumComplementario = Math.round(100 - ((roundedStarAVGNum / 5) * 100));
-                response.render("dashboard", {
-                    titulo: 'Dashboard',
-                    promedioPuntajes: averageScores,
-                    tasaDeRespuesta: responseRates,
-                    encuestasEnviadas: reviewsSent,
-                    numAVGEstrella: roundedStarAVGNum,
-                    porcentaje: roundedStarAVGNumComplementario,
-                    marca: marca,
-                    ruta: "/graphics/dashboard",
-                    errorProducto: undefined,
-                    permisos: request.session.permisos || []
-                });
-            })
-            .catch(error => {
-                console.error('Error obteniendo datos para el dashboard:', error);
-                next(error); // Pasa el error al siguiente middleware para manejarlo
-            });
-    };
+    if(producto !== "*") {
 
-        
+        try {
+            const result = await Model.search(producto);
+                
+            if(result.error !== undefined) {
+                errorMessage = result.error;
+                producto = "*";
+            
+            } else {
+                errorMessage = result.error;
+            }
+                
+        } catch(error) {
+            console.log(error)
+        }
+            
+    }
+    
+    Promise.all([Model.StarAvgLine(marca,categoria), Model.tasaDeRespuesta(marca,categoria,producto),Model.ReviewsSentxMonth(marca,categoria,producto),Model.StarAvgNumber(marca,categoria,producto)])
+        .then(([averageScores, responseRates,reviewsSent,starAVGNum]) => {
+            const roundedStarAVGNum = Math.round(starAVGNum * 100) / 100
+            const roundedStarAVGNumComplementario = Math.round(100 - ((roundedStarAVGNum / 5) * 100));
+            response.render("dashboard", {
+                titulo: 'Dashboard',
+                promedioPuntajes: averageScores,
+                tasaDeRespuesta: responseRates,
+                encuestasEnviadas: reviewsSent,
+                numAVGEstrella: roundedStarAVGNum,
+                porcentaje: roundedStarAVGNumComplementario,
+                marca: marca,
+                ruta: "/graphics/dashboard",
+                errorProducto: errorMessage,
+            });
+        })
+        .catch(error => {
+            console.error('Error obteniendo datos para el dashboard:', error);
+            next(error); // Pasa el error al siguiente middleware para manejarlo
+        });
 
 };
 
