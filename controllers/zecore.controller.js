6cc0cdff38bce327c02d436a084c3007c50391a5
Juan Pablo <a01705408@tec.mx>
2024-04-18 19:15:42 -0600

    Zecore puede: Enviar Venta, Enviar Nuevo Producto, Modificar Producto. Se utilizó una API key falsa, habrá que cambiarla

--- controllers/zecore.controller.js
+++ controllers/zecore.controller.js
@@ -1,12 +1,7 @@
-const ZecoreHelper = require('../models/zecorehelper.model');
-const zh = require('../models/zecorehelper.model')
+const ZecoreSaleHelper = require('../models/zecoresaleshelper.model');
+const ZecoreProductHelper = require('../models/zecoreproducthelper.model')
 
-
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-
-const validateToken = (req, res, next) => {
+exports.validateToken = (req, res, next) => {
     const token = req.headers["authorization"];
     // validar token
     if (!token || token !== 'Prueba') {
@@ -15,20 +10,47 @@ const validateToken = (req, res, next) => {
     // token ok, seguir
     next();
 };
+exports.post_ModifyProduct = (request,response,next) =>{
+    const MP = request.body;
+    const ModifyProduct = new ZecoreProductHelper();
+    ModifyProduct.ModificarProducto(MP.idProducto,MP.Columna,MP.NuevoValor).then(()=>{
+        return response
+        .status(200)
+        .json({message: 'Hemos recibido la información'});
+    }).catch((error) =>{
+        console.log('Error al recibir la información '+ error);
+        return response
+        .status(500)
+        .json({message: "Error al recibir la información "+ error });
+    });
 
+}
+
+exports.post_NewProduct = (request, response, next) =>{
+    const data = request.body;
+    const {idProducto,marcaProducto,Nombre,Image,Descripcion,categoria} = data;
+    const NEWproduct = new ZecoreProductHelper(idProducto,marcaProducto,Nombre,Image,Descripcion,categoria);
+    NEWproduct.RegistrarProducto()
+        .then(() => {
+            return response
+            .status(200)
+            .json({message: 'Hemos recibido la información'});
+        })
+        .catch((error) => {
+            console.log('Error al recibir la información ' + error);
+            return response
+            .status(500)
+            .json({message: "Error al recibir la información "+ error });
+        });
+}
 
 exports.post_venta = (request, response, next) =>{
-    console.log('gg')
-  validateToken;
   const data = request.body;
   const { Cliente, Producto_id, Fecha, SalesON } = data;
-  const venta = new ZecoreHelper(Cliente, Producto_id, Fecha, SalesON);
+  const venta = new ZecoreSaleHelper(Cliente, Producto_id, Fecha, SalesON);
   console.log(venta)
-  console.log('gg2')
-
   venta.RegistrarVenta()
         .then(() => {
-            console.log('gg3')
             return response
             .status(200)
             .json({
