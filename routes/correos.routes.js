d6f5c6305400cac22cb0e110b4ed3e5711b781ae
Angie <A01705651@tec.mx>
2024-04-23 21:47:43 -0600

    crear_pregunta_final

--- routes/correos.routes.js
+++ routes/correos.routes.js
@@ -8,5 +8,5 @@ router.get("/correos/editar/:marca/:pregunta_id", isAuth, canEdit, controladores
 router.post('/correos/editar/', controladores.post_editar_correos);
 router.get("/correos/:marca", isAuth, canEdit, controladores.get_correos);
 router.get("/correos/crear/:marca", isAuth, canEdit, controladores.get_correos_crear);
-router.post('/correos/crear/', controladores.post_crear_correos);
+router.post('/correos/crear/:marca', controladores.post_crear_correos);
 module.exports = router;
