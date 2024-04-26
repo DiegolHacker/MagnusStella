0986e875c58b2a5754063f539cbacb56bacdcdd7
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-04-26 10:54:57 -0600

    cambio a passport-setup

--- passportSetup.js
+++ passportSetup.js
@@ -1,14 +1,14 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const userModel = require('./models/usuarios.model');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const userModel = require("./models/usuarios.model");
 
 //Prueba123
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.ClientIDGoogle,//poner manana un .env para esto
-      clientSecret: process.env.ClientSecretGoogle ,
-      callbackURL: 'http://localhost:3000/users/auth/google/callback',
+      clientID: process.env.ClientIDGoogle, //poner manana un .env para esto
+      clientSecret: process.env.ClientSecretGoogle,
+      callbackURL: "/users/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -21,11 +21,11 @@ passport.use(
           return done(null, user);
         } else {
           // User not found, return false to deny authentication
-          console.log('Error el usuario no se encontro')
+          console.log("Error el usuario no se encontro");
           return done(null, false);
         }
       } catch (err) {
-        console.error('Error in google strategy callback: ', err);
+        console.error("Error in google strategy callback: ", err);
         return done(err, null);
       }
     }
