c9226feef5340679974b425a2c65caf5572d7122
Pablo Hazael Hurtado Mireles <h.h.m.pablo@gmail.com>
2024-04-28 23:34:16 -0600

    Template de Correos casi terminada

--- controllers/resenas.controller.js
+++ controllers/resenas.controller.js
@@ -1,5 +1,19 @@
 const Reviews = require("../models/resenas.model");
 const Correos = require("../models/correos.model");
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+
+//Definimos las variables para nodemailer
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "tracpablo@gmail.com",
+    pass: "gjlu ultc ppfm srbq",
+  },
+});
+
 
 exports.get_resenas_completas = (request, response, next) => {
   const marca = request.params.marca;
@@ -71,10 +85,11 @@ exports.enviar_resenia = async (request, response, next) => {
   console.log(csrfToken);
 
   try {
+
+    //Recuperamos los valores para el template de resenias
     const total = await Correos.emailpreguntas(marca); // Espera a que se resuelva la promesa
 
     const { preguntas, idp } = await Correos.emailConfiguration(marca);
-
     const tipos = [];
     const opciones = [];
     const total_opciones = [];
@@ -87,175 +102,66 @@ exports.enviar_resenia = async (request, response, next) => {
       total_opciones.push(to_opcion);
     }
 
-    let html = ``;
+    console.log('Antes de ejs')
+    //Renderizamos el template del ejs
+    ejs.renderFile('views/prueba_correo.ejs',{
+      preguntas: preguntas,
+      titulo: "Correos",
+      marca: marca || "LU1",
+      ruta: "/reviews/resenas/enviar_resenia/LU1",
+      csrfToken: request.csrfToken(),
+      idp: idp,
+      total: total,
+      tipos: tipos,
+      opciones: opciones,
+      idp: idp,
+      total_opciones: total_opciones,
+      permisos: request.session.permisos || [],
+      attachments: [
+        {
+          filename: "image.png",
+          path: `./public/images/${marca}.png`,
+          cid: "unique@nodemailer.com", //same cid value as in the html img src
+        },
+      ],
 
-    html += `
+    }, function(err, template){
+      if (err) {
+        console.log('Error: ', err)
+
+      } else {
+
+        console.log('Dentro de ejs')
+
+        //Usamos main para enviar los correos
+        async function main() {
+
+          // send mail with defined transport object
+          const info = await transporter.sendMail({
+
+            from: '"Forgot password" <tracpablo@gmail.com>', // sender address
+            to: "a01710778@tec.mx", // list of receivers
+            subject: "Intento1", // Subject line
+            html: template,
+            attachments: [
+              {
+                filename: "image.png",
+                path: `./public/images/${marca}.png`,
+                cid: "unique@nodemailer.com", //same cid value as in the html img src
+              },
+            ],
+
+          });
 
 
-        <body>
-        <div class="preview-html">
-            <div class="contenido">
-                <form action="http://localhost:3000/users/login" method="POST">
-                <input type="hidden" name="_csrf" value="${csrfToken}" />
+          console.log("Message sent: %s", info.messageId);
+          // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+        }
 
-                <img src="cid:unique@nodemailer.com" height="200" width="100%">
-                <h4><b>Hola user,</b></h4>
-                <p id="sub_question">Has comprado recientemente 'producto' <br> ¿Nos cuentas tu experiencia?</p>
-                <div class="rating-box">
-                    <h5 id="question_text"><b>¿Cuál es su calificación general?</b></h5>
-                    <div class="rating">
-                      <input type="radio" name"clr1">
-                      <input type="radio" name"clr1">
-                      <input type="radio" name"clr1">
-                      <input type="radio" name"clr1">
-                      <input type="radio" name"clr1">
-                    </div>
-                </div>
-                <h4><b>Título de tu reseña: </b></h4>
-                <input type="text" id="title_rev" name="titulo_review" placeholder="Título"><br>
-                <h4><b>Reseña: </b></h4>
-                <textarea name="descripcion_review" id="description_rev" cols="35" rows="4" placeholder="Escribe aquí tu reseña"></textarea>
-                <div id="survey-form">
-        `;
-    for (var i = 0; i < preguntas.length; i++) {
-      html += `
-            <br>
-            <h4><b>${preguntas[i].descripcion}</b></h4>
-            `;
-      if (tipos[i][0].tipo == 1) {
-        html += `
-                <p id="sub_question">Elige una de las siguientes opciones</p>
-                                
-                <select name="answer-${preguntas[i].idPregunta}">`;
-
-        opciones[i].forEach((opcion) => {
-          html += `<input type="radio" name="gender" value="${opcion.idOpcion}" checked> ${opcion.descripcion}<br>`;
-        });
-        html += `</select>
-                `;
-      } else if (tipos[i][0].tipo == 2) {
-        opciones[i].forEach((opcion) => {
-          html += `
-                    <textarea name="answer-${preguntas[i].idPregunta}" id="option-${opcion.idOpcion}" cols="35" rows="3" placeholder="${opcion.descripcion}"></textarea>
-                    <br>
-                    `;
-        });
-      } else if (tipos[i][0].tipo == 3) {
-        html += `
-                <p id="sub_question">Elige una o varias de las siguientes opciones</p>
-                `;
-        opciones[i].forEach((opcion) => {
-          html += `
-                    <input type="checkbox" id="option-${opcion.idOpcion}" name="answer-${preguntas[i].idPregunta}" value="${opcion.idOpcion}">
-                    <label for="option-${opcion.idOpcion}">${opcion.descripcion}</label><br>
-                    `;
-        });
+        //Mandamos a crear el correo y si falla nos manda el error
+        main().catch(console.error);
       }
-    }
-
-    html += `
-                </div>
-                
-                <div class="submit-button-container">
-                    <button type="submit" class="submit-button">¡Envía tu reseña!</button>
-                </div>
-            </form>
-
-            </div>
-        </div>
-
-      <link rel="stylesheet" href="/css/style.css" />
-      <link rel="stylesheet" href="/css/correos.css" />
-
-
-        <style>
-        @import ("https://fonts.googleapis.com/css?family=Roboto|Varela+Round")
-        @import ("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css")
-        @import ("https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css")
-
-        .rating:not(:checked) > input {
-          position: absolute;
-          appearance: none;
-        }
-        
-        .rating:not(:checked) > label {
-          float: right;
-          cursor: pointer;
-          font-size: 30px;
-          color: #666;
-          
-        }
-        
-        .rating:not(:checked) > label:before {
-          content: '★';
-        }
-        
-        .rating > input:checked + label:hover,
-        .rating > input:checked + label:hover ~ label,
-        .rating > input:checked ~ label:hover,
-        .rating > input:checked ~ label:hover ~ label,
-        .rating > label:hover ~ input:checked ~ label {
-          color: #fdd300;
-        }
-        
-        .rating:not(:checked) > label:hover,
-        .rating:not(:checked) > label:hover ~ label {
-          color: #fdca00;
-        }
-        
-        .rating > input:checked ~ label {
-          color: #fdd300;
-          
-        }
-        
-        .rating-box .rating {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: nowrap;
-          flex-direction: row-reverse;
-        }
-        </style>
-
-        <div>
-          <p> algo </p>
-        </div>
-
-        </body>`;
-
-    const nodemailer = require("nodemailer");
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: "tracpablo@gmail.com",
-        pass: "gjlu ultc ppfm srbq",
-      },
-    });
-
-    async function main() {
-      // send mail with defined transport object
-      const info = await transporter.sendMail({
-        from: '"Forgot password" <tracpablo@gmail.com>', // sender address
-        to: "a01710778@tec.mx", // list of receivers
-        subject: "Intento1", // Subject line
-        html: html,
-        attachments: [
-          {
-            filename: "image.png",
-            path: `./public/images/${marca}.png`,
-            cid: "unique@nodemailer.com", //same cid value as in the html img src
-          },
-        ],
-      });
-
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-    }
-
-    main().catch(console.error);
+    })
 
     // Ahora renderiza con los datos obtenidos
     response.render("prueba_correo", {
@@ -263,6 +169,7 @@ exports.enviar_resenia = async (request, response, next) => {
       titulo: "Correos",
       marca: marca || "LU1",
       ruta: "/reviews/resenas/enviar_resenia/LU1",
+      csrfToken: request.csrfToken(),
       idp: idp,
       total: total,
       tipos: tipos,
