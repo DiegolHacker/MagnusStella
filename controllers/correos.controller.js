bc2c2687c0e2a056f2b111b741e7e4703cc42a97
Diego <a01705506@tec.mx>
2024-04-29 21:36:44 -0600

    [FEAT] Eliminar preguntas funcional con ajax.

--- controllers/correos.controller.js
+++ controllers/correos.controller.js
@@ -161,6 +161,17 @@ exports.post_crear_correos = (request, response, next) => {
     });
 };
 
+exports.borrar_pregunta = async (request, response, next) => {
+  const idPregunta = request.params.pregunta_id;  //Toma el id de la pregunta de la ruta.
+  try {
+    await Correos.delete(idPregunta);
+  } catch (error) {
+    console.log("Error al borrar:", error);
+    response.status(500).send("Error interno del servidor");
+  }
+
+}
+
 exports.post_dias = (request, response, next) => {
   
 }
