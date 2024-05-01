81bdfc938e0495b87099bbc8d9c00f935fa0c9e8
Diego Alfaro Pinto <a01709971@tec.mx>
2024-05-01 07:26:58 +0000

    cron job no funciona

--- CRON_job/cronJob.js
+++ CRON_job/cronJob.js
@@ -1,2 +1,10 @@
-// Log "Hello cron job" to the console
-console.log("Hello cron job");
+const fs = require('fs');
+
+// Function to append text to a file
+function appendToFile(filename, text) {
+    fs.appendFileSync(filename, text + '\n');
+}
+
+// Log "Hello cron job"
+appendToFile('cron_job_log.txt', 'Hello cron job');
+
