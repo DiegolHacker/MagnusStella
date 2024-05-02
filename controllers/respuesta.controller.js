6d27780a73a56b06c8351607e5536403b193e887
Juan Pablo <a01705408@tec.mx>
2024-05-01 18:18:28 -0600

    console.logs eliminados

--- controllers/respuesta.controller.js
+++ controllers/respuesta.controller.js
@@ -1,31 +1,5 @@
 const respuestaM = require("../models/respuesta.model");
 
-// exports.post_MailResponse = (request, response, next) => {
-//   console.log(request.body);
-
-//   const resp = request.body;
-
-//   const respuesta = new respuestaM({
-//     nombre: resp.nombre,
-//     email: resp.email,
-//     mensaje: resp.mensaje,
-//   });
-
-//   respuesta
-//     .save()
-//     .then(() => {
-//       return response
-//         .status(200)
-//         .json({ message: "Se ha registrado la respuesta" });
-//     })
-//     .catch((error) => {
-//       console.log("Error al recibir respuesta: " + error);
-//       return response
-//         .status(500)
-//         .json({ message: "Error al recibir información: " + error });
-//     });
-// };
-
 exports.post_MailResponse = async (request, response, next) => {
   const resp = request.body;
   rating = resp.rate;
@@ -35,7 +9,6 @@ exports.post_MailResponse = async (request, response, next) => {
   obj = new respuestaM()
   idrev = await obj.CreateReview(idventa,desc,title,rating);
   for (const clave in resp){
-    console.log(clave)
     if (clave.startsWith('answer_')){
       const idPregunta = parseInt(clave.split('_')[1]);
       const respuestas = resp[clave];
@@ -50,7 +23,5 @@ exports.post_MailResponse = async (request, response, next) => {
     }
   }
 }
-  console.log('FIN')
   response.redirect("/");
-  console.log(request.body);
 };
