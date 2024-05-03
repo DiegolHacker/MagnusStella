e745aba15cfc72e5d63cc21f258fbf342abf68d7
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-05-02 19:32:40 -0600

    cambio a cronjob

--- CRON_job/cronJob.controller.js
+++ CRON_job/cronJob.controller.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const model = require("./cronJob.model");
 const controller = require("../controllers/resenas.controller");
 
-module.exports = cron.schedule("0 15 * * *", () => {
+module.exports = cron.schedule("0/5 * * * *", () => {
   model
     .getVentasTime()
     .then(([mailInfo, fieldData]) => {
